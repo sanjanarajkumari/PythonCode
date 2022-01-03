ff15fbcc14f180463d3b30830f9ae41bb9567936
sanjanarajkumari <61216746+sanjanarajkumari@users.noreply.github.com>
2022-01-03 21:29:46 +0530

    code2

---
+++ code2.py
@@ -0,0 +1,5 @@
+#write a program to find gross pay with 35hr and rate of 2.75/hrs.
+hours = input("Enter no. of hours = ")
+rate = input("Enter rate per hour = ")
+grosspay = float(hours) * float(rate)
+print(grosspay)
