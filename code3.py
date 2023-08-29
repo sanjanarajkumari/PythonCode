d894d1c4670df7b9a1ce36503c5fc011de9697cc
sanjanarajkumari <61216746+sanjanarajkumari@users.noreply.github.com>
2023-08-29 19:51:32 +0530

    update1

---
+++ code3.py
@@ -0,0 +1,7 @@
+import pandas
+
+#Read and work with a file named 'industry.csv'
+df = pandas.read_csv('industry.csv')
+print(df)
+#This will display first few rows from the industry.csv file
+#csv- Comma-separated values file
---
+++ code4.py
@@ -0,0 +1,4 @@
+print(" I want to die as the day declines,at high sea and facing the sky, while agony seems like a dream and my soul like a bird that can fly.")
+print(3j)
+print(type(3j))
+print(type(1.05))
