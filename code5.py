46b481fa13a85098e31ca5890d6a234b7af02523
sanjanarajkumari <61216746+sanjanarajkumari@users.noreply.github.com>
2023-11-29 16:32:45 +0530

    updated code

---
+++ code5.py
@@ -0,0 +1,39 @@
+#Examples of Python Single-Line Comments
+
+#This is a single line comment
+print("This is a print statement.")
+print("Hello World!!!")  #Printing hello World
+print("Python Program")
+#print("Python Program")
+
+#Examples of Python Multi-line Comments-The use of '#'
+# It will execute a block of code if a specified condition is true.
+# If the condition is false then it will execute another block of code.
+p = 7
+if p < 5:
+  print("p is smaller than 5")
+else:
+  print("p is not greater than 5")
+
+#Examples of Python Multi-line Comments-The use of multiline string
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+p = 7
+if p > 5:
+  print("p is greater than 5.")
+else:
+  print("p is not greater than 5.")
+
+# print("This doesnt "execute")
+print("This will \" execute")
+
+print("Hey I am \"Sanjana\"\nand I am a good girl")
+
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
+
+print("hey",7,6)
+print("hey",7,6,sep="~")
+print("hey",7,6,sep="~",end="007")
+print("Hello world")
+print("Hello world")
